0a60b935001ac348620c6449340c278793c557c4
krishna5867 <krishnakmr968@gmail.com>
2024-09-17 17:12:11 +0530

    user logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
-import { signOut } from 'next-auth/react'
+'use client'
+
+import { signOut, useSession } from 'next-auth/react'
 import Link from 'next/link'
 import { redirect } from 'next/navigation';
 import React from 'react'
-import { getServerSession } from "next-auth";
 
 
-const Header = async () => {
-  const session = await getServerSession();
+const Header = () => {
+  const { data: session, status } = useSession();
 
   const handleLogout = () => {
     signOut();
@@ -21,21 +22,24 @@ const Header = async () => {
             <h2 className='text-white font-semibold'>Accounting App</h2>
           </Link>
         </div>
+     <div className='text-white'>
+     </div>
         <div className='flex gap-x-4'>
-          {/* {session ? 
-          <button className="bg-black text-white px-4 py-1.5 rounded-full border">
+          {session ? 
+          <button className="bg-black text-white px-4 py-1.5 rounded-full border"
+          onClick={()=> signOut()}>
             Sign Out
           </button> 
           : (
-            <> */}
+            <>
               <Link href="/signin">
                 <button className='bg-black text-white px-4 py-1.5 rounded-full border'>Login</button>
               </Link>
               <Link href="/register">
                 <button className='bg-black text-white px-4 py-1.5 rounded-full border'>SignUp</button>
               </Link>
-            {/* </> */}
-          {/* )} */}
+            </>
+          )}
         </div>
       </div>
     </div>
