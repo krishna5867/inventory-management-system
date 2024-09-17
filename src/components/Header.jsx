71d1796f1ac222eebfce45ee643bcfedafa63b3d
krishna5867 <krishnakmr968@gmail.com>
2024-09-17 17:42:43 +0530

    logout implemented

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,34 +15,35 @@ const Header = () => {
   }
 
   return (
-    <div className='w-full bg-black'>
-      <div className='flex items-center justify-between px-4 py-2'>
-        <div>
-          <Link href="/">
-            <h2 className='text-white font-semibold'>Accounting App</h2>
-          </Link>
-        </div>
-     <div className='text-white'>
-     </div>
-        <div className='flex gap-x-4'>
-          {session ? 
-          <button className="bg-black text-white px-4 py-1.5 rounded-full border"
-          onClick={()=> signOut()}>
-            Sign Out
-          </button> 
-          : (
-            <>
-              <Link href="/signin">
-                <button className='bg-black text-white px-4 py-1.5 rounded-full border'>Login</button>
-              </Link>
-              <Link href="/register">
-                <button className='bg-black text-white px-4 py-1.5 rounded-full border'>SignUp</button>
-              </Link>
-            </>
-          )}
+      <div className='w-full bg-black'>
+        <div className='flex items-center justify-between px-4 py-2'>
+          <div>
+            <Link href="/">
+              <h2 className='text-white font-semibold'>Accounting App</h2>
+            </Link>
+          </div>
+          <div className='text-white'>
+          </div>
+          <div className='flex gap-x-4'>
+            {session ?
+              <button className="bg-black text-white px-4 py-1.5 rounded-full border"
+                onClick={() => signOut()}>
+                Sign Out
+              </button>
+              : (
+                <>
+                  <Link href="/signin">
+                    <button className='bg-black text-white px-4 py-1.5 rounded-full border'>Login</button>
+                  </Link>
+                  <Link href="/register">
+                    <button className='bg-black text-white px-4 py-1.5 rounded-full border'>SignUp</button>
+                  </Link>
+                </>
+              )}
+          </div>
         </div>
       </div>
-    </div>
+
   )
 }
 
